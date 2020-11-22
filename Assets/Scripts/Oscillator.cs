1aa99d0f0a271652a75908bc1b8942f6eebf595b
aditya-167 <adityasrichandan3098@gmail.com>
2020-11-23 00:41:33 +0530

    Movement of platforms in game

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Vector3 movement;
+
+    [Range(0, 1)] [SerializeField] float movementFactor;
+
+    Vector3 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementFactor * movement;
+        transform.position = startPos + offset;
+    }
+}
