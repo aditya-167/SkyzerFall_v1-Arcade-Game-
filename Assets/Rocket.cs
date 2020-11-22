f438956e31008cc8bc44499f96ac876c6062e39a
aditya-167 <adityasrichandan3098@gmail.com>
2020-11-22 17:22:09 +0530

    Rocket script addtion of sounds

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,10 +7,13 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
+              
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,12 +27,20 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if(audioSource.isPlaying == false)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
-            print("Rotating Left");
+            transform.Rotate(Vector3.forward);
 
-        else if(Input.GetKey(KeyCode.D))
-            print("Rotating Right");
+        else if (Input.GetKey(KeyCode.D))
+            transform.Rotate(-Vector3.forward);
     }
 }
